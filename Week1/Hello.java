73f6add0246938ebd1160f1aca75e87057d8ab81
niraj <nrajchand2085@gmail.com>
2024-04-14 12:18:31 +0545

    week 1 commit

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,6 +2,13 @@ package Week1;
 
 public class Hello {
     public static void main(String[] args) {
+        // Write a program that prints Hello World! to the console. Save the program as Hello.java.
         System.out.println("Hello world");
+        // Extend/Modify the above program to print two additional lines:
+        // This is my first program
+        // I am on module ST4003CEM
+        // System.out.print (not println) and produces the same output.
+
+        System.out.print("This is my first code\nI am on module ST4003CEM");
     }
 }
