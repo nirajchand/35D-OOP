8464bab6b8b9e3b3be685defa04f2f09b514b18b
niraj <nrajchand2085@gmail.com>
2024-04-30 14:14:01 +0545

    Task week1

--- Tasks/Week1.java
+++ Tasks/Week1.java
@@ -38,7 +38,7 @@ public class Week1 {
 
         // WAP that prints the following output:
         // A well-formed Java program has a main method with { and } braces.
-
+        
 
         // A System.out.println statement has ( and ) and usually a String that 
         // starts and ends with a " character.
